0cca40763aeae5b195134e6b8fc8b82ed6bf4ea9
Александр Данильченко <mlemix32@gmail.com>
2022-04-20 23:40:49 +0300

    Добавьте файлы проекта.

---
+++ avg.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1;
+	int num2;
+	int num3;
+
+	cout << "\tEnter first num : ";
+	cin >> num1;
+
+	cout << "\tEnter second num : ";
+	cin >> num2;
+
+	cout << "\tEnter third num : ";
+	cin >> num3;
+
+	cout << "Average number : " << (num1 + num2 + num3) / 3 << "\n";
+
+}
